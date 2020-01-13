3707776881561662dacbeec0d367e12b26192c0c
うにナッツ <imnotchestnuts@gmail.com>
2020-01-13 20:49:45 +0900

    measure buttons redirect to index

--- visa_controller/urls.py
+++ visa_controller/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'visa_controller'
+
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('action_index/', views.action_index, name='action_index'),
 ]
--- visa_controller/views.py
+++ visa_controller/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    print("index")
     return render(request, 'visa_controller/index.html')
+
+def action_index(request):
+    redirect_url = reverse('visa_controller:index')
+    print(redirect_url)
+    if request.method == 'GET':
+        if 'VPP' in request.GET:
+            print("cout: VPP")
+        if 'VMAX' in request.GET:
+            print("cout: VMAX")
+    return redirect('visa_controller:index')
